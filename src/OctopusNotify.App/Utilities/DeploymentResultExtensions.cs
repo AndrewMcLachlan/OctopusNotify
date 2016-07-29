1bcc024f7d8f246343f8b8b3a249fcd1d0d01ae9
Andrew McLachlan <amc_home@hotmail.com>
2016-07-29 22:29:32 +1000

    Checked in DeploymentResultExtensions separately to avoid Git getting confused. Added mapping from deployment result to notification.

--- src/OctopusNotify.App/Utilities/DeploymentResultExtensions.cs
+++ src/OctopusNotify.App/Utilities/DeploymentResultExtensions.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Octopus.Client.Model;
+using System.Drawing;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using OctopusNotify.App.Models;
+using OctopusNotify.App.Properties;
 using OctopusNotify.Model;
 
 namespace OctopusNotify.App.Utilities
@@ -15,6 +15,49 @@ namespace OctopusNotify.App.Utilities
             if (deploymentResult == null) return null;
 
             return String.Format("{0}, {1}, {2}", deploymentResult.Project.Name, deploymentResult.Version, deploymentResult.Environment.Name);
+        }
+
+        public static Notification ToNotification(this DeploymentResult deploymentResult)
+        {
+            var notification = new Notification
+            {
+                EnvironmentName = deploymentResult.Environment.Name,
+                ProjectName = deploymentResult.Project.Name,
+                Version = deploymentResult.Version,
+                Link =  new Uri(String.Format("{0}/app#/deployments/{1}", Settings.Default.ServerUrl.TrimEnd('/'), deploymentResult.DeploymentId))
+            };
+
+            switch(deploymentResult.Status)
+            {
+                case DeploymentStatus.Success:
+                    notification.Icon = BalloonIcon.Success;
+                    notification.Message = "Deployment Succeeded";
+                    break;
+                case DeploymentStatus.Failed:
+                case DeploymentStatus.FailedNew:
+                    notification.Icon = BalloonIcon.Error;
+                    notification.Message = "Deployment Failed";
+                        break;
+                case DeploymentStatus.TimedOut:
+                case DeploymentStatus.TimedOutNew:
+                    notification.Icon = BalloonIcon.Error;
+                    notification.Message = "Deployment Timed out";
+                    break;
+                case DeploymentStatus.Fixed:
+                    notification.Icon = BalloonIcon.Success;
+                    notification.Message = "Deployment Fixed";
+                    break;
+                case DeploymentStatus.ManualStep:
+                    notification.Icon = BalloonIcon.Question;
+                    notification.Message = "Manual Action Required";
+                    break;
+                case DeploymentStatus.GuidedFailure:
+                    notification.Icon = BalloonIcon.Warning;
+                    notification.Message = "Guided Failure Requires Attention";
+                    break;
+            }
+
+            return notification;
         }
     }
 }
