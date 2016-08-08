ae81fe6778ef5f29b110f892ece5005812d3ff34
Andrew McLachlan <amc_home@hotmail.com>
2016-08-08 17:41:40 +1000

    Updated version numbers.

--- src/OctopusNotify.App/Properties/AssemblyInfo.cs
+++ src/OctopusNotify.App/Properties/AssemblyInfo.cs
@@ -52,8 +52,8 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.*")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.*")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
 
 #if STUB
 [assembly: XmlnsDefinition("test-mode", "Namespace")]
